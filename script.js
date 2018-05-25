bd55161c96494c0686051e1da795fcf6fea41406
koltunburke-imac <stephkoltun@icloud.com>
2018-05-25 17:14:16 -0400

    added revised split screen

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 var mapData;
 
 var currentSettings = {
-  mode: "split",
+  mode: "split-single",
   isDragging: false,
-  splits: 1
+  splits: 1,
+  isSplit: false,
 }
 
 // get JSON data about maps
@@ -163,17 +164,75 @@ function createPane(classname, map) {
   return element
 }
 
+
+function splitSingle(properties) {
+  let index = parseInt(properties.number) - 1;
+  let map = mapData[index];
+
+  $('.grid-item img').css({'border-bottom': '3px solid #000', 'border-right': '3px solid #000'});
+
+  $('.grid-wrapper').css({"width": "40%", "height": "50vh", "display": "inline-block", "float": "left", "overflow": "scroll", "border-right": "3px solid #000", "border-bottom": "3px solid #000", "border-top": "6px solid #000", "border-left": "6px solid #000"});
+
+  $('.grid-sizer, .grid-item').css({'width': '48%', 'height': "150px"});
+  $('.grid').isotope('layout');
+
+  let video = "<div class='vid'>" + map.video + "</div>";
+  $('body').append(video);
+
+  let post = "<div class='post'>" + "<h1>" + map.title + "</h1>" + map.post + "</div>"
+  $('body').append(post);
+
+  let images = "<div class='images'>";
+  for (var i = 0; i < map.images.length; i++) {
+    images += "<img src='img/" + map.images[i] + "'/>";
+  }
+  images += "</div>";
+  $('body').append(images);
+
+  let groups = "<div id='groups'><h2>Groups</h2></div>";
+  $('body').append(groups);
+
+  let w = $("#groups").width();
+
+  let size = w/3*2;
+  let padding = 10;
+
+  for (var i = 0; i < map.groups.length; i++) {
+    let group = "<div class='grp' style='height:" + (size + padding*2*(map.groups[i].length+1)) + "px'>"
+
+    for (var k = 0; k < map.groups[i].length; k++) {
+      let offset = (k)*padding;
+      let groupitem = "<div class='group-item' style='height:" + size + "px;top:" + offset*2 + "px;left:" + offset + "px'></div>";
+      group += groupitem
+    }
+    group += "</div>"
+    $('#groups').append(group);
+  }
+}
+
 $('.grid').on('click', '.grid-item', function() {
   switch (currentSettings.mode) {
     case "popup":
       if ($('.popup').length == 0) {
-        $('.grid').css('opacity',"0.2");
+        $('.grid').css('opacity',0.2);
       }
       showPopup($(this)[0].dataset);
       break;
     case "split":
       currentSettings.splits++;
       splitScreen($(this)[0].dataset);
+      break;
+    case "split-single":
+      if (currentSettings.isSplit) {
+        $('.post').remove();
+        $('.images').remove();
+        $('.vid').remove();
+        $('#groups').remove();
+      }
+      $('.grid-item').css('opacity', .5);
+      $(this).css('opacity', 1);
+      splitSingle($(this)[0].dataset);
+      currentSettings.isSplit = true;
       break;
     default:
       console.log("default");
